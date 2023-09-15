376ae801598d7f0707d24ded0aac9791f5140245
Daniel Ide Futata <daniel.futata@makes.ai>
2023-09-15 16:17:41 -0300

    indent

--- sql.py
+++ sql.py
@@ -1,13 +1,12 @@
+import sqlite3
 from langchain.llms import LlamaCpp
 from langchain.utilities import SQLDatabase
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-
 from prompts import _DEFAULT_TEMPLATE
 
-db = SQLDatabase.from_uri("sqlite:///db/analysis.db")  # Load the SQLite database
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])  # Initialize the callback manager
 llm = LlamaCpp(
     model_path="models/wizard.gguf",
@@ -23,10 +22,6 @@ prompt = PromptTemplate(
     input_variables=["input"], template=_DEFAULT_TEMPLATE
 )
 chain = LLMChain(llm=llm, prompt=prompt)
-
-# manual query db
-
-import sqlite3
 
 if __name__=='__main__':
     conn_query = sqlite3.connect('db/queries.db')
