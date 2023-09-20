22c3d296de5a7beb03ddb3bfddd0686bcb3c7075
danft123 <danielrag@gmail.com>
2023-09-20 13:10:04 -0300

    fix gpt feature

--- sql.py
+++ sql.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from langchain.llms import LlamaCpp
+from langchain.llms import LlamaCpp, OpenAI
 from langchain.utilities import SQLDatabase
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
@@ -8,19 +8,16 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from prompts import _DEFAULT_TEMPLATE
 from constants import *
-from openai import OpenAI
 
 def choose_model():
-    USE_GPT4 = os.getenv('USE_GPT4', 'False').lower() in ['true', '1', 't', 'y', 'yes']
-    if USE_GPT4:
+    CHOOSE_MODEL = input("7b, 13b, gpt4?\n")
+    if 'gpt' in CHOOSE_MODEL:
         print("Using GPT-4 model")
-        return "/path/to/gpt4/model"
+        return "gpt4"
+    if "7" in CHOOSE_MODEL:
+        return "/home/shared/airoboros-l2-7b-2.2.Q4_K_M.gguf"
     else:
-        CHOOSE_MODEL = input("7b or 13b?")
-        if "7" in CHOOSE_MODEL:
-            return "/home/shared/airoboros-l2-7b-2.2.Q4_K_M.gguf"
-        else:
-            return "/home/shared/airoboros-l2-13b-2.2.Q4_K_M.gguf"
+        return "/home/shared/airoboros-l2-13b-2.2.Q4_K_M.gguf"
 
 def connect_to_db(db_name):
     conn = sqlite3.connect(db_name)
@@ -37,6 +34,7 @@ if __name__=='__main__':
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])  # Initialize the callback manager
     MODEL_PATH = choose_model()
     if 'gpt' in MODEL_PATH:
+        print("\nUsing OpenAI api key...\n")
         llm = OpenAI(temperature=0.0)
     else:
         llm = LlamaCpp(
@@ -58,7 +56,7 @@ if __name__=='__main__':
         QUERY = {'input': query, 'metavars':metavars, 'text1':text1, 'text2':text2}
         response = chain.run(QUERY)
         update_analysis(c_analysis, id, response)
-    conn_query.commit()
-    conn_analysis.commit()
-    conn_query.close()
-    conn_analysis.close()
+    c_query.commit()
+    c_analysis.commit()
+    c_query.close()
+    c_analysis.close()
