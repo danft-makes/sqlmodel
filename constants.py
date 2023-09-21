a9d41ccc2c0487a1b8671c94ebdd6f0ba195e14f
Daniel Ide Futata <daniel.futata@makes.ai>
2023-09-21 16:59:11 -0300

    before merge skeleton

--- constants.py
+++ constants.py
@@ -7,12 +7,12 @@ if prompt_maker==True:
     text1="""{SYSTEM_RULES}"""
     text2="""{HARD_RULE}"""
 else:
-    metavars="""Table inventory columns: [inventory_id,product_id,branch_id,quantity]
-    Table sales columns: [sale_id,sale_date,customer_id,branch_id,product_id,quantity,total_price]
-    Table customer has columns: [customer_id,creation_date,rfm_class,name,city,phone,last_purchase_date]
-    Table branch columns: [branch_id,manager_id,city]
-    Table product columns: [product_id,name,category,creation_date,description]
-    JOINS: [sales.customer_id = customer.customer_id, sales.branch_id = branch.branch_id, sales.product_id = product.product_id, inventory.product_id = product.product_id, inventory.branch_id = branch.branch_id, branch.manager_id = manager.manager_id]"""
+    metavars="""Table manager columns: [manager_id,name,performance_class]
+    Table sales columns: [sale_id,sale_date,quantity,total_price,customer_id,branch_id,product_id]
+    Table customer has columns: [customer_id,creation_date,rfm_class,name,phone,last_purchase_date,city]
+    Table branch columns: [branch_id,city,manager_id]
+    Table product columns: [product_id,name,creation_date,description,category]
+    Foreign keys: [sales.customer_id = customer.customer_id, sales.branch_id = branch.branch_id, sales.product_id = product.product_id, branch.manager_id = manager.manager_id]"""
     text1="""Never query for all the columns from a specific table, only ask for the relevant columns given the question.
     DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc...).
     If the question does not seem related to the database, just return "I don't know".
@@ -20,3 +20,4 @@ else:
     text2="""Answer ONLY a sqlite query that answers the question."""
 
 instruction=metavars+'\n'+text1+'\n'+text2
+db_info="""∣ manager : manager_id , name , performance_class ∣ sales : sale_id , sale_date , quantity , total_price , customer_id , branch_id , product_id ∣ customer : customer_id , creation_date , rfm_class , name , phone , last_purchase_date , city ∣ branch : branch_id , city , manager_id ∣ product : product_id , name , creation_date , description , category ∣ sales.customer_id = customer.customer_id ∣ sales.branch_id = branch.branch_id ∣ sales.product_id = product.product_id ∣ branch.manager_id = manager.manager_id ∣"""
--- experiment.py
+++ experiment.py
@@ -6,7 +6,7 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from prompts import _CLOSEINSTRUCTION_TEMPLATE, _ALPACA_TEMPLATE
+from configs.prompts import _CLOSEINSTRUCTION_TEMPLATE, _ALPACA_TEMPLATE
 from constants import *
 from utils import ModelChooser, DatabaseManager
 from db import ALLMODELS
